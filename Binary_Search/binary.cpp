b352f685b88246ba843a22ded1754f594a2a5c6c
Samudrala Hasini <Samudrala.Hasini@iiitb.ac.in>
2025-06-27 15:49:33 +0530

    added binary

---
+++ Binary_Search/binary.cpp
@@ -0,0 +1,33 @@
+/*
+Problem link - https://leetcode.com/problems/binary-search/description/
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int low=0;
+       int high = nums.size()-1;
+
+       while(low <= high)
+       {
+        int mid = (low+high)/2;
+
+        if(nums[mid]==target)
+        return mid;
+
+        else if(nums[mid]>target)
+        high = mid-1;
+
+        else
+        low = mid+1;
+
+       }
+        return -1;
+    }
+};
+
+//Intution
+/*
+basic binary serach which takes logn time to complete and then it is used maximum only when teh array is sorted either in ascedng order or in descdeing order 
+
+*/
