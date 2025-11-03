8e1c265556e47f29a5d3736764b41a999a3c7584
Samudrala Hasini <Samudrala.Hasini@iiitb.ac.in>
2025-11-04 01:06:46 +0530

    modified

--- Arrays/max_consecutive.cpp
+++ Arrays/max_consecutive.cpp
@@ -30,3 +30,25 @@ Here we need to find teh maximum num of consecutive ones so we took a varibale a
 a number other than one then we have to check the max and teh count of number of 2's till there and 
 then try to change the max and return the max aat the end
 */
+
+
+//approach - 2
+class Solution{
+    public:
+    int findMaxConsecutiveOnes(vector<int>&nums){
+        int next =0;
+        int prev = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]!=1){
+                prev = max(prev,next);
+                next =0;
+            }
+            else{
+                next++;
+            }
+        }
+        prev = max(next,prev);
+        return prev;
+    }
+};
